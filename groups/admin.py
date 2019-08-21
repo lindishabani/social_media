e049bb686a63693c2467b9725dc35bb231f9c2e4
linds <lindshabani97@hotmail.com>
2019-08-21 11:55:57 +0200

    finishing groups app

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMembers
+
+
+admin.site.register(models.Group)
+
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from .models import Group, GroupMembers
 
-# Create your views here.
+
+class CreateGroup(generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DeleteView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
